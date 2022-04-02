a5426e824a4812baf960215d7b4c4f999f05d80a
jym0404 <jym090404@naver.com>
2022-04-02 20:43:54 +0900

    Update bot.py

--- bot.py
+++ bot.py
@@ -10,7 +10,7 @@ async def on_ready():
     print('logged in')
 
 engdap = {"조랭봇 안녕":"안녕하세요!","조랭봇 뭐해":"저는 사실 기계 안에서 노동을 하고 ㅇ... 살려주세요!!!","조랭봇 사랑해":"에?","조랭봇 사귀자":"에?","조랭봇 꺼져":"싫어요 켜져있을거예요","조랭봇 랭조":"||어쩔티비||","조랭봇 mrpdjxmtld":"축하합니다! 당신은 이스터에그를 발견하셨습니다!\n고로 당신은 크리님의 서버에 들어올 수 있습니다\nhttps://discord.gg/gSuSAN2","조랭봇 크리멍청이":"아 님 멍청이라고요?","조랭봇 ㅎㅇ":"안녕하세요.","조랭봇 ㅂㅇ":"안녕히계세요.","조랭봇 이스터에그":"그걸 알려줄것 같냐? ㅋㅋㅋ.","조랭봇 조랭봇":"저는 발달된 인공지능은 아니고 그냥 커맨드형식으로 만들어진 봇입니다","조랭봇 자기소개":"저는 크리님에 의해 만들어진 응답 봇이랍니다.","조랭봇 크리":"저를 만드신 분이예요!"}
-
+deunglok = {}
 #조랭봇 멤버 개발하기
 #for member in message.guilds.members: 써서
 
@@ -25,11 +25,32 @@ async def on_message(message):
                 if not key == "조랭봇 mrpdjxmtld":
                     now = now + (key+": "+value+"\n-----------------------------------\n")
             await message.channel.send(now)
+        elif message.startswith("조랭봇 등록"):
+            nowdeunglok = message.content[6:]
+            nowdeunglok = nowdeunglok.split(' ')
+            nowdeunglok = [nowdeunglok[0]," ".join(nowdeunglok[1:])]
+            deunglok[nowdeunglok[0]] = [nowdeunglok[1],message.author.name]
+            await message.channel.send(nowdeunglok[0]+"은(는) "+nowdeunglok[1]+" 이군요! 알려 주셔서 감사해요!")
+            await message.channel.send("부적절한 말을 등록하였을시 관리자에게 직접 삭제됩니다")
+            
+            author = await client.get_user("725688321992294501").create_dm()
+            await author.send(nowdeunglok)
+        elif message.startswith("조랭봇 삭제"):
+            if message.author.id == "725688321992294501" or message.author.id == 725688321992294501:
+                try:
+                    del(deunglok[message.content[7:]])
+                except:
+                    await message.channel.send("Error 404 can't find expected string")
+            else:
+                await message.channel.send("관리자만 이용할 수 있는 기능입니다!")
         else:
-        	try:
-        		await message.channel.send(engdap[message.content])
-        	except:
-        		await message.channel.send("아직 모르는 단어예요!")
-        
+            try:
+                await message.channel.send(engdap[message.content])
+            except:
+                try:
+                    await message.channel.send(deunglok[message.content[:5]][0])
+                    await message.channel.send(deunglok[message.content[:5]][1]+"님이 알려주셨어요!")
+                except:
+                    await message.channel.send("아직 모르는 단어예요!")
 client.run(os.environ['token'])
 
