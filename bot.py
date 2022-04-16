0cc7ca3406599c9bd11d1092709f37a2c834158f
jym0404 <jym090404@naver.com>
2022-04-16 21:18:22 +0900

    Update bot.py

--- bot.py
+++ bot.py
@@ -55,7 +55,7 @@ async def on_message(message):
             except:
                 await message.channel.send("정확히 멘션을 해주세요!")
             await message.channel.send(message)
-            user = message.message.server.get_member(userid)
+            user = message.message.guild.get_member(userid)
             await message.channel.send(user)
             date = datetime.datetime.utcfromtimestamp(((int(user.id) >> 22) + 1420070400000) / 1000)
             await message.channel.send(f"{message.author.mention}\n가입일: {date.year}/{date.month}/{date.day}\n닉네임: {user.name}\n서버 닉네임: {user.display_name}\n아이디: {user.id}")
