8ffc2aa66e136b2c00871c929816f504a42c1c27
jym0404 <jym090404@naver.com>
2022-07-20 14:08:55 +0900

    Update bot.py

--- bot.py
+++ bot.py
@@ -34,7 +34,7 @@ async def on_message(message):
             if checkope == len(calcstr):
                 try:
                     await message.channel.send("정답은 "+str(eval(calcstr))+"이에요!")
-                except MemoryError:
+                except OverflowError:
                     await message.channel.send("정답은... 펑:boom:")
                 except:
                     await message.channel.send("올바르지 못한 형식이에요!")
